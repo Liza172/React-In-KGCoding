8e165cfe94e4bffbfce331a9f19049dcbc163a5d
Liza172 <lizarakshit@gmail.com>
2025-01-19 12:06:17 +0530

    Social Media3

--- Projects/6 Social Media/src/Components/Post.jsx
+++ Projects/6 Social Media/src/Components/Post.jsx
@@ -1,16 +1,33 @@
+import { AiFillDelete } from "react-icons/ai";
+import PostList from "./PostList";
 
+import {PostList} from "../Store/Port-list-store";
+import { useContext } from "react";
 const Post = ({post}) =>
 {
+
+  const {deletePost} = useContext(PostList);
   return (
-    <div className="card post-card" style={{width: "18rem"}}>
+    <div className="card post-card">
   
     <div className="card-body">
-      <h5 className="card-title">{post.title}</h5>
+
+      <h5 className="card-title">{post.title}
+
+        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+        onClick = {() => deletePost(post.id)}>
+          <AiFillDelete/>
+          <span className="visually-hidden">unread messages</span>
+      </span>
+      </h5>
       <p className="card-text">{post.body} </p>
       {post.tags.map((tag) => 
       {
-        <span className = "badge text-bg-primary">{tag}</span>
+        <span key = {tag} className = "badge text-bg-primary">{tag}</span>
       })}
+      <div className="alert alert-info reaction" role="alert">
+       This Post has been reacted by {post.reactions} people...!!!
+      </div>
     </div>
   </div>
   )
